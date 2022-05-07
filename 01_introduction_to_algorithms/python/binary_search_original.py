f3d69b3b7456b3d0af1305567875694af409c87c
vladimir-asus <vladimir@vladimir-e200ha>
2022-05-08 00:53:02 +0500

    Modification binary_search_original.py

--- 01_introduction_to_algorithms/python/binary_search_original.py
+++ 01_introduction_to_algorithms/python/binary_search_original.py
@@ -10,18 +10,18 @@ def binary_search(lst, item):
         guess = lst[mid]
         # Found the item.
         if guess == item:
-            return True
+            return mid
         # The guess was too high.
         if guess > item:
             high = mid - 1
         # The guess was too low.
         else:
             low = mid + 1
-    return False  # Item doesn't exist
+    return None  # Item doesn't exist
 
 
 my_list = [1, 3, 5, 7, 9]
-print(binary_search(my_list, 1))  # => True
+print(binary_search(my_list, 1))  # => 0
 
 # 'None' means nil in Python. We use to indicate that the item wasn't found.
-print(binary_search(my_list, -1))  # => False
+print(binary_search(my_list, -1))  # => None
